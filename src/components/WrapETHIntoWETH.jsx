3c8683ab57ca0db20e9688bc7392c10c3e10c0f6
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-24 20:40:34 +0300

    WrapETHIntoWETH component

--- src/components/WrapETHIntoWETH.jsx
+++ src/components/WrapETHIntoWETH.jsx
@@ -1,11 +1,8 @@
-import {
-  Box,
-  Typography,
-} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 function WrapETHIntoWETH() {
   return (
-    <Box sx={{'margin-top': '50px'}}>
+    <Box sx={{ marginTop: "50px" }}>
       <Typography variant="h2">Wrap ETH to WETH:</Typography>
     </Box>
   );
