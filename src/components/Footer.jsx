80a4d9e6f239f38d5babe53792476b6f76d54bf2
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-24 20:09:41 +0300

    create basic footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
+import {Container, Typography} from "@mui/material";
+
 function Footer() {
-  return <h1>Footer</h1>;
+  return <Container disableGutters maxWidth={false} sx={{position: 'fixed', bottom: '0px', left: '15px'}}>
+  <Typography variant='p' component='p'>React | Blockchain</Typography>
+</Container>;
 }
 
 export default Footer;
