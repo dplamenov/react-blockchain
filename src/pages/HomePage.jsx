932afdcc3240bf90855e3aaf3a00a4fa27f3f40e
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-24 20:55:44 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
+import { useSelector } from "react-redux";
+import { Typography } from "@mui/material";
 import { Stats, WrapETHIntoWETH } from "../components";
 
 function HomePage() {
+  const { account } = useSelector((state) => state.web3);
+
   return (
     <>
-      <Stats />
-      <WrapETHIntoWETH />
+      {!!account ? (
+        <>
+          <Stats />
+          <WrapETHIntoWETH />
+        </>
+      ) : (
+        <>
+          <Typography variant="h2">Not connect to blockchain.</Typography>
+        </>
+      )}
     </>
   );
 }
