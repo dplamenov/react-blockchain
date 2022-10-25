dff1b300ed89a9da14bdb9aee730986b273a41db
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-25 21:39:43 +0300

    update WrapETHIntoWETH component

--- src/components/WrapETHIntoWETH.jsx
+++ src/components/WrapETHIntoWETH.jsx
@@ -1,9 +1,32 @@
-import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 function WrapETHIntoWETH() {
+  const [amountToWrap, setAmountToWrap] = useState(0);
+
+  const {
+    contracts: { weth },
+  } = useSelector((state) => state.web3);
+
+  const wrapHandler = () => {
+    weth.deposit({ value: amountToWrap });
+  };
+
   return (
     <Box sx={{ marginTop: "50px" }}>
       <Typography variant="h2">Wrap ETH to WETH:</Typography>
+      <Box sx={{ display: "flex", gap: "20px" }}>
+        <TextField
+          label="Amount ETH to wrap"
+          variant="outlined"
+          value={amountToWrap}
+          onChange={(e) => setAmountToWrap(e.target.value)}
+        />
+        <Button variant="contained" onClick={wrapHandler}>
+          Wrap
+        </Button>
+      </Box>
     </Box>
   );
 }
