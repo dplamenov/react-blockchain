f14d17fdf1079ecf261babe4cf02e32504c06190
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-25 21:10:53 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Connect } from "./Connect";
 export { default as Stats } from "./Stats";
 export { default as WrapETHIntoWETH } from "./WrapETHIntoWETH";
+export { default as Loader } from "./Loader";
