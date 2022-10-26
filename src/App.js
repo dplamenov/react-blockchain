631529630d53f2568345ef8f814bc69dafbb8fe5
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-26 09:07:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Provider } from "react-redux";
 import { Box } from "@mui/material";
+import { NotificationContainer } from "react-notifications";
+import "react-notifications/lib/notifications.css";
 import { store } from "./stores/store";
 import { HomePage } from "./pages";
 import { Connect, Footer, Header } from "./components";
-import "react-notifications/lib/notifications.css";
-import { NotificationContainer } from "react-notifications";
 
 function App() {
   return (
