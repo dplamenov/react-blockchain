f467da7e24b65e26ebb5f2378a99efb2939d4d2d
Dimitar Dimitrov <dimitar.plamenov@gmail.com>
2022-10-26 15:52:08 +0300

    Update connect.js

--- src/connect.js
+++ src/connect.js
@@ -3,7 +3,7 @@ import { connect, loadContracts } from "./stores/web3";
 import contracts from "./contracts";
 
 export default async ({ dispatch }) => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
   const accounts = await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
   dispatch(
@@ -25,4 +25,19 @@ export default async ({ dispatch }) => {
       },
     })
   );
+
+  const chainId = (await provider.getNetwork()).chainId;
+
+  if(chainId !== 1) {
+    window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x1" }],
+    });
+  }
+
+  provider.on("network", (_, oldNetwork) => {
+    if (oldNetwork) {
+      window.location.reload();
+    }
+  });
 };
